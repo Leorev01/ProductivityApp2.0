8d73c4bb2e251b1997b1eae50ce153b7425055e6
Leorev01 <revrennaleo@gmail.com>
2025-06-27 14:46:02 +0100

    Adds add habit page

--- app/(auth)/dashboard/components/HabitsPage.tsx
+++ app/(auth)/dashboard/components/HabitsPage.tsx
@@ -1,39 +1,19 @@
 import { PlusIcon } from "@heroicons/react/16/solid";
 import Habits from "./Habit";
-
-const habits = [
-    {
-        id: 1,
-        title: "Drink Water",
-        description: "Drink at least 2 liters of water daily.",
-    },
-    {
-        id: 2,
-        title: "Exercise",
-        description: "30 minutes of exercise every day.",
-    },
-    {
-        id: 3,
-        title: "Read",
-        description: "Read at least 20 pages of a book daily.",
-    },
-    {
-        id: 4,
-        title: "Meditate",
-        description: "Meditate for 10 minutes every morning.",
-    },
-]
+import { habits } from "@/utils/habits"; // Assuming you have a data file with habits
+import Link from "next/link";
 const HabitsPage = () => {
   return (
     <div className="flex flex-col gap-4">
         <div className="flex flex-row items-center gap-4 p-6 justify-between">
             <h1 className="text-2xl font-bold text-gray-900">Habits</h1>
-            <button
+            <Link
+                href="/dashboard/habits/add"
                 type="button"
                 className="rounded-full bg-indigo-600 p-2 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
             >
                 <PlusIcon aria-hidden="true" className="size-5" />
-            </button>
+            </Link>
         </div>
         {habits.map((habit) => (
             <Habits key={habit.id} title={habit.title} description={habit.description} />
---
+++ utils/habits.ts
@@ -0,0 +1,22 @@
+export const habits = [
+    {
+        "id": 1,
+        "title": "Drink Water",
+        "description": "Drink at least 2 liters of water daily."
+    },
+    {
+        "id": 2,
+        "title": "Exercise",
+        "description": "30 minutes of exercise every day."
+    },
+    {
+        "id": 3,
+        "title": "Learn French",
+        "description": "Do at least 20 minutes of Duolingo in French daily."
+    },
+    {
+        "id": 4,
+        "title": "Meditate",
+        "description": "Meditate for 10 minutes every morning."
+    }
+]
