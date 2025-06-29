15d0eabf87f4c2eff0767c67d90871d4bd80675e
Leorev01 <revrennaleo@gmail.com>
2025-06-29 21:06:50 +0100

    Updates fetchUser function

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -12,10 +12,24 @@ const __dirname = path.dirname(__filename)
 
 //Fetch User function
 const fetchUser = async (req: Request, res: Response) => {
+    const userId = req.query.id as string;
+    if (!userId) {
+        return res.status(400).json({ 
+            success: false, 
+            error: 'User ID is required' 
+        });
+    }
     try{
-        const filePath = path.join(__dirname, '../data/user.json')
+        const filePath = path.join(__dirname, '../data/users.json')
         const data = await fs.promises.readFile(filePath, 'utf-8')
-        const userData: User = JSON.parse(data)
+        const users: User[] = JSON.parse(data)
+        const userData = users.find(user => user.id === parseInt(userId))
+        if (!userData) {
+            return res.status(404).json({ 
+                success: false, 
+                error: 'User not found' 
+            });
+        }
         res.status(200).json(userData)
     }catch(error){
         console.error('Error fetching user:', error);
